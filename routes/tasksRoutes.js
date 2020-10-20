83c0f67db4394f61c951cead773eda0cda0f89d4
Matthew Mulford <mhmulford0@gmail.com>
2020-10-20 13:04:17 -0500

    added country to reg process. added user route to get classes. fixed delete

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -73,7 +73,7 @@ router.post("/", async (req, res) => {
 });
 
 
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const task = await db("tasks").where({ id });
